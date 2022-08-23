b52d882cb7bb0654ab41b5a5b54688b036e6d304
Bruno Camillo <101357397+BrunoXimenezCamillo@users.noreply.github.com>
2022-08-23 17:39:46 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,8 @@ const AuthMiddleware = require('./app/Middlewares/AuthMiddleware')
 const UserController = require('./app/Controllers/User_Controller')
 const loginController = require('./app/Controllers/Login_Controller')
 const productController = require('./app/Controllers/Product_Controller')
-
+const categoryController = require('./app/Controllers/Category_Controller');
+const Category = require('./app/Models/Category');
 
 //===================
 // ROUTES
@@ -19,10 +20,14 @@ const routes = new Router();
 
 routes.post('/register', UserController.store);
 routes.post('/login', loginController.index);
+
 routes.get('/account', AuthMiddleware, UserController.myAccount);
 routes.post('/account/create-product', AuthMiddleware, UserController.createProduct);
 routes.post('/account/edit-product', AuthMiddleware, UserController.editProduct);
 routes.post('/account/delete-product', AuthMiddleware, UserController.deleteProduct);
+routes.post('/account/create-category', AuthMiddleware, categoryController.createCategory);
+
+routes.get('/category', categoryController.getCategories);
 
 routes.get('/products', productController.findProducts);
 
