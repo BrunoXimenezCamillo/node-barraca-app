2dbc18685cf385372b80084a2189acfe693c23ca
Bruno Camillo <101357397+BrunoXimenezCamillo@users.noreply.github.com>
2022-08-23 17:39:32 -0300

    Add files via upload

--- src/app/Controllers/Category_Controller.js
+++ src/app/Controllers/Category_Controller.js
@@ -6,6 +6,26 @@ class Category_Controller {
         var data = await Category.find();
         return res.status(200).json({ data });
     }
+
+    async createCategory(req, res) {
+        var schema = yup.object().shape({
+            name: yup.string().required()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({
+                error: true,
+                message: 'Invalid data'
+            })
+        }
+
+        var { data } = req.body;
+
+        Category.create(data, err => err ?
+            res.status(400).json({ error: true, message: "Failed to register category" }) :
+            res.status(200).json({ error: false, message: "Sucess to register category" }));
+
+    }
 }
 
 module.exports = new Category_Controller();
